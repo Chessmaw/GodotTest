501e1afc2e69a380afb5a5710b52f3a73954524f
Chessmaw <85143601+Chessmaw@users.noreply.github.com>
2023-07-17 19:07:50 -0600

    TestDeMovimento

--- Escenas/Personaje.gd
+++ Escenas/Personaje.gd
@@ -1,16 +1,25 @@
+#Cuerpo del Personaje
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("Hello Wolrd")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#Velocidad del Jugador
+export (float) var Velocidad = 200
+#Indentificacion del Personaje
+var VelocidadDelCuerpo = Vector2()
+#Funcion de entrada de Teclas
+func EntradadeTeclas():
+	var VelocidadDelCuerpo = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		VelocidadDelCuerpo.x += 1
+	if Input.is_action_pressed("ui_left"):
+		VelocidadDelCuerpo.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		VelocidadDelCuerpo.y += 1
+	if Input.is_action_pressed("ui_up"):
+		VelocidadDelCuerpo.y -= 1
+	VelocidadDelCuerpo = VelocidadDelCuerpo.normalized() * Velocidad
+	
+func _physics_process(delta):
+	EntradadeTeclas()
+	VelocidadDelCuerpo = move_and_slide(VelocidadDelCuerpo)
